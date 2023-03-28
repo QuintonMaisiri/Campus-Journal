d27c8c21508b023546b62b7d99aef993ef67f323
Quinton <maisiriquinton@gmail.com>
2023-03-28 15:26:53 +0200

    fixed articles section

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("user", userSchema);
 
+// const commentSchema = new mongoose.Schema({
+//     user : User,
+//     comment : String,
+//     article : Article,
+// });
+
+// const Comment = new mongoose.model("comment",commentSchema);
 
 
 passport.use(User.createStrategy());
@@ -138,6 +145,30 @@ app.get("/compose-setup", (req, res) => {
 app.post("/compose", (req, res) => {
     res.render("compose", { title: req.body.articleName, journal: req.body.journal })
 });
+
+app.post("/comment",(req ,res) =>{
+
+    const article = new Article();
+    const user = new User();
+
+    User.findOne({username:req.username}).then((foundUser)=>{
+        user = foundUser;
+    })
+
+    Article.findOne({_id:req.id}).then((foundArticle)=>{
+        article = foundArticle;
+    });
+
+    const comment = new Comment({
+        user : user ,
+        comment : req.comment,
+        article : article
+    })
+
+    comment.save();
+    res.send("200");
+
+})
 
 app.post("/savepost", (req, res) => {
     const article = new Article({
